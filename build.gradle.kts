f9b7c671c8adb99841fd94eda853bed1cd13b994
Hui <huienlin.game@gmail.com>
2024-10-27 14:27:24 +0100

    add: add jetbrain exposed library for database interaction

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 val postgres_version: String by project
 val h2_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -35,6 +36,14 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+
+    // Jetbrains exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
+
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
