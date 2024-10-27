6ffbff0e907853c9738b81009e1909b061b0900c
Hui <huienlin.game@gmail.com>
2024-10-27 17:30:08 +0100

    update: protect some routes

--- src/main/kotlin/com/huiun/routes/UserRoutes.kt
+++ src/main/kotlin/com/huiun/routes/UserRoutes.kt
@@ -5,6 +5,7 @@ import com.huiun.controller.request.UserCreationRequest
 import com.huiun.controller.request.UserSetPasswordRequest
 import com.huiun.controller.request.UserUpdateRequest
 import io.ktor.http.*
+import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -28,58 +29,56 @@ fun Route.userRoutes() {
         }
     }
 
-    get("api/v1/user/{id}") {
-        try {
+    authenticate {
+        get("api/v1/user/{id}") {
+            try {
+                val id = call.parameters["id"]?.toString() ?: throw IllegalArgumentException("Invalid ID")
+
+                val response = userController.findUserById(id)
+                call.respond(HttpStatusCode.OK, response)
+            } catch (e: Exception) {
+                call.application.environment.log.error("Unexpected error occur", e)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+
+        put("api/v1/user/{id}/password") {
+            try {
+                val id = call.parameters["id"]?.toString() ?: throw IllegalArgumentException("Invalid ID")
+
+                val request = call.receive<UserSetPasswordRequest>()
+                val response = userController.setPassword(id, request)
+
+                call.respond(HttpStatusCode.OK, response)
+            } catch (e: Exception) {
+                call.application.environment.log.error("Unexpected error occur", e)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+
+        put("api/v1/user") {
+            val request = call.receive<UserUpdateRequest>()
+
+            try {
+                val response = userController.updateUserInfo(request)
+
+                call.respond(HttpStatusCode.OK, response)
+            } catch (e: Exception) {
+                call.application.environment.log.error("Unexpected error occur", e)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+
+        delete("api/v1/user/{id}") {
             val id = call.parameters["id"]?.toString() ?: throw IllegalArgumentException("Invalid ID")
 
-            val response = userController.findUserById(id)
-            call.respond(HttpStatusCode.OK, response)
+            try {
+                userController.deleteUser(id)
+            } catch (e: Exception) {
+                call.application.environment.log.error("Unexpected error occur", e)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+            TODO("Handle deleting a user")
         }
-        catch (e: Exception) {
-            call.application.environment.log.error("Unexpected error occur", e)
-            call.respond(HttpStatusCode.InternalServerError)
-        }
-    }
-
-    put("api/v1/user/{id}/password") {
-        try {
-            val id = call.parameters["id"]?.toString() ?: throw IllegalArgumentException("Invalid ID")
-
-            val request = call.receive<UserSetPasswordRequest>()
-            val response = userController.setPassword(id, request)
-
-            call.respond(HttpStatusCode.OK, response)
-        }
-        catch (e: Exception) {
-            call.application.environment.log.error("Unexpected error occur", e)
-            call.respond(HttpStatusCode.InternalServerError)
-        }
-    }
-
-    put("api/v1/user") {
-        val request = call.receive<UserUpdateRequest>()
-
-        try {
-            val response = userController.updateUserInfo(request)
-
-            call.respond(HttpStatusCode.OK, response)
-        }
-        catch (e: Exception) {
-            call.application.environment.log.error("Unexpected error occur", e)
-            call.respond(HttpStatusCode.InternalServerError)
-        }
-    }
-
-    delete("api/v1/user/{id}") {
-        val id = call.parameters["id"]?.toString() ?: throw IllegalArgumentException("Invalid ID")
-
-        try {
-            userController.deleteUser(id)
-        }
-        catch (e: Exception) {
-            call.application.environment.log.error("Unexpected error occur", e)
-            call.respond(HttpStatusCode.InternalServerError)
-        }
-        TODO("Handle deleting a user")
     }
 }
