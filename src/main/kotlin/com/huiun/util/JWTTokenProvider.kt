0aaf9cfc34f145b31eb8593f651924e6dd9af665
Hui <huienlin.game@gmail.com>
2024-10-27 17:06:45 +0100

    add: implement JWTTokenProvider for token related features

---
+++ src/main/kotlin/com/huiun/util/JWTTokenProvider.kt
@@ -0,0 +1,32 @@
+package com.huiun.util
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.huiun.model.User
+import java.util.*
+
+class JWTTokenProvider(
+    private val secretKey: String,
+    private val expiration: Long,
+    private val issuer: String,
+    private val audience: String,
+    private val myRealm: String
+) {
+
+    val validator = JWT
+        .require(Algorithm.HMAC256(secretKey))
+        .withAudience(audience)
+        .withIssuer(issuer)
+        .build()
+
+    fun getRealm(): String = myRealm
+
+    fun generateToken(user: User): String {
+        return JWT.create()
+            .withAudience(audience)
+            .withIssuer(issuer)
+            .withClaim("username", user.username)
+            .withExpiresAt(Date(System.currentTimeMillis() + expiration))
+            .sign(Algorithm.HMAC256(secretKey))
+    }
+}
