7b53f5db4401bcc14f3d24744b9a6f29300120ba
Hui <huienlin.game@gmail.com>
2024-10-27 16:46:14 +0100

    update: implement user controller features

--- src/main/kotlin/com/huiun/controller/UserController.kt
+++ src/main/kotlin/com/huiun/controller/UserController.kt
@@ -2,15 +2,17 @@ package com.huiun.controller
 
 import com.huiun.controller.request.UserCreationRequest
 import com.huiun.controller.request.UserSetPasswordRequest
+import com.huiun.controller.request.UserUpdateRequest
 import com.huiun.controller.response.UserResponse
 import com.huiun.model.User
 import com.huiun.service.UserService
+import java.util.*
 
 interface UserController {
     suspend fun createUser(userCreationRequest: UserCreationRequest): UserResponse
     suspend fun findUserById(id: String): UserResponse
-    suspend fun updateUserInfo(): UserResponse
-    suspend fun setPassword(userSetPasswordRequest: UserSetPasswordRequest)
+    suspend fun updateUserInfo(userUpdateRequest: UserUpdateRequest): UserResponse
+    suspend fun setPassword(id: String, userSetPasswordRequest: UserSetPasswordRequest)
     suspend fun deleteUser(id: String)
 }
 
@@ -18,23 +20,37 @@ class UserControllerImpl(
     private val userService: UserService
 ) : UserController {
     override suspend fun createUser(userCreationRequest: UserCreationRequest): UserResponse {
-        TODO("Not yet implemented")
+        val user = userService.createUser(
+            userCreationRequest.email,
+            userCreationRequest.username,
+            userCreationRequest.password
+        )
+
+        return UserResponse(user.id, user.email, user.username)
     }
 
     override suspend fun findUserById(id: String): UserResponse {
-        TODO("Not yet implemented")
+        val user = userService.findUserById(UUID.fromString(id))
+
+        return UserResponse(user.id, user.email, user.username)
     }
 
-    override suspend fun updateUserInfo(): UserResponse {
-        TODO("Not yet implemented")
+    override suspend fun updateUserInfo(userUpdateRequest: UserUpdateRequest): UserResponse {
+        val user = userService.updateUserInfo(
+            userUpdateRequest.id,
+            userUpdateRequest.email)
+
+        return UserResponse(user.id, user.email, user.username)
     }
 
-    override suspend fun setPassword(userSetPasswordRequest: UserSetPasswordRequest) {
-        TODO("Not yet implemented")
+    override suspend fun setPassword(id: String, userSetPasswordRequest: UserSetPasswordRequest) {
+        userService.setNewPassword(
+            UUID.fromString(id),
+            userSetPasswordRequest.oldPassword, userSetPasswordRequest.newPassword)
     }
 
     override suspend fun deleteUser(id: String) {
-        TODO("Not yet implemented")
+        userService.deleteUser(UUID.fromString(id))
     }
 
 
