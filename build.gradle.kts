ae705952a0bba1442ae2e1b94bcbc28913b16c40
Hui <huienlin.game@gmail.com>
2024-10-27 14:35:06 +0100

    add: add kotlin coroutines test for testing suspend functions

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+
+    // Jbcrypt
     implementation("org.mindrot:jbcrypt:0.4")
 
     // Jetbrains exposed
@@ -56,4 +58,5 @@ dependencies {
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
